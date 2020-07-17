777dc939bf2e37a98ec3687b953ef780ebc1ffb4
ASHUTOSH KUMAR <kr.ashu456@gmail.com>
2020-07-18 04:18:46 +0530

    Updates

---
+++ precache-manifest.2a0df1491dd7ffaf979618d5fa480d9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc25a95066fa1546538a1282f2f9be26",
+    "url": "/expense-tracker/index.html"
+  },
+  {
+    "revision": "a3aa889f6d7792abb6b3",
+    "url": "/expense-tracker/static/css/main.9580823e.chunk.css"
+  },
+  {
+    "revision": "b82325b433a8d1607535",
+    "url": "/expense-tracker/static/js/2.a597404b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/expense-tracker/static/js/2.a597404b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3aa889f6d7792abb6b3",
+    "url": "/expense-tracker/static/js/main.ff02b4ba.chunk.js"
+  },
+  {
+    "revision": "cb8ae26c37fb294b27ca",
+    "url": "/expense-tracker/static/js/runtime-main.3a0c4ee0.js"
+  }
+]);
